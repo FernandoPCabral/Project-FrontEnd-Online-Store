f7e9f17411fe11727908cd2b9ac91b123ae41148
fernando <pcabral.fernando@gmail.com>
2022-02-09 17:14:43 -0300

    requisito 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,10 +4,10 @@ class Home extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" />
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
+        <input type="text" />
       </div>
     );
   }
