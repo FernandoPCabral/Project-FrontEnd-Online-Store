f4382bb15fe36280786d7e08f5871470458182fd
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-02-09 16:18:54 -0300

    Req 1

--- src/services/api.js
+++ src/services/api.js
@@ -2,16 +2,14 @@ export async function getCategories() {
   const url = 'https://api.mercadolibre.com/sites/MLB/categories';
   const resp = await fetch(url);
   const data = await resp.json();
+
   return data;
 }
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
-  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
   const resp = await fetch(url);
   const data = await resp.json();
 
-  const url2 = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
-  const resp2 = await fetch(url2);
-  const data2 = await resp2.json();
-  return data || data2;
+  return data;
 }
