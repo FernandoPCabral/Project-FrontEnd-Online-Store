332649142f3eab14017cec8c9844e9faf27eb88c
higor cavalcante dos anjos <higorc.anjos@gmail.com>
2022-02-10 20:34:51 -0300

    pull

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/Cart" component={ Cart } />
+          <Route exact path="/Details/:id" component={ Details } />
         </Switch>
       </BrowserRouter>
     </div>
